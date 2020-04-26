e440c091a2cca272b9dd6be5942141abf8621912
Tatyana Alekseeva <tanichalex@mail>
2020-04-26 15:08:15 +0300

    Домашняя работа 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,58 +1,49 @@
 "use strict";
-let a = 2;
-let b = 4;
-let c = -3;
-let d = b**2 - 4 * a * c;
-let x = [];
-function getResult(a,b,c){
-  if (d > 0) {
-    x[0] = -b - Math.sqrt(d) / (2 * a);
+let a;
+let b;
+let c;
+
+function getResult(a, b, c) {
+  let d = b**2 - 4 * a * c;
+  let x = [];
+  if (d < 0) {
+    x = [];
+  } else if (d === 0) {
+    x[0] = -b / (2 * a);
+  } else if ( d > 0) {
+    x[0] = (-b - Math.sqrt(d)) / (2 * a);
     x[1] = (-b + Math.sqrt(d)) / (2 * a);
-  } else if (d = 0) {
-    x[0] = -b / (2 * a)
-  } else if (d < 0) {
-      x = [];
-  }
-    // код для задачи №1 писать здесь
-    return x;
+  };
+  return x;
 };
 
 let marks = [];
-marks.push(3, 5, 4, 3, 5, 3);
-let markss;
-let sum = 0;
-let averageRating = 0;
-
-function getAverageMark(marks){
-  if (marks.length > 5) {
-    console.log("Колличество оценок " + marks.length);
-    markss = marks.slice(0, 5);
-    for (let index = 0; index < markss.length; index++) {
-       sum += markss[index];
-      averageMark = sum / markss.length;
-    }; 
-  } else if (marks.length <= 5) {
-    for (let index = 0; index < marks.length; index++) {
-      sum += marks[index];
-      averageMark = sum / marks.length;
-    }; 
-  } else if (marks.length = 0) {
+function getAverageMark(marks) {
+  let sum = 0;
+  let averageMark;
+  let averageRating = 0;
+  if (marks.length === 0) {
     averageMark = 0;
-  }
+  } else if (marks.length > 5) {
+    console.log("Колличество оценок " + marks.length);
+    marks = marks.slice(0, 5);
+  };
+  for (let index = 0; index < marks.length; index++) {
+    sum += marks[index];
+    averageMark = sum / marks.length;
+    };
   return averageMark;
-}; 
+  };
 
-let name = "Nik";
-let dateOfBirthday = 2010;
-let age = new Date().getFullYear() - dateOfBirthday;
+let name;
+let dateOfBirthday;
 let result;
 
-function askDrink(name,dateOfBirthday){
+function askDrink(name, dateOfBirthday){
+  let age = new Date().getFullYear() - dateOfBirthday;
   if (age > 18) {
     result = `Не желаете ли олд-фэшн, ${name}?`
   }
   else (result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`)
   return result;
-  // код для задачи №3 писать здесь
-    // return result;
-}
+};
